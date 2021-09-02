25fa7f2925746a3edf9287ae1ce4187e3aebe246
as8610194625 <as8610194625@gmail.com>
2021-09-02 16:15:01 +0800

    clean up

--- bookstw.py
+++ bookstw.py
@@ -3,9 +3,10 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import time
 
-url = "https://search.books.com.tw/search/query/cat/1/sort/1/v/0/page/1/spell/3/ms2/ms2_1/key/python"
+url = "https://cdn.kingstone.com.tw/english/images/product/8726/9783030188726.jpg"
 headers = {'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'}
 
 res = requests.get(url,headers=headers)
 soup = BeautifulSoup(res.text,"html.parser")
+# article = soup.select
 print(soup)
