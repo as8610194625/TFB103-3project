22a1445162f23b2ff479885157a5f26c9201f94d
as8610194625 <as8610194625@gmail.com>
2021-11-23 20:32:54 +0800

    ..

--- crewdatas_functions/python_mongodb_stored.py
+++ crewdatas_functions/python_mongodb_stored.py
@@ -1,9 +1,9 @@
 from pymongo import MongoClient, collection
 import json
 import os
-connection = MongoClient(host='127.0.0.1',port=27017)
+connection = MongoClient(host='10.2.14.10',port=27017)
 db = connection.kingstone
-collection = db['test']
+collection = db['cleanbook']
 def kingstone_stored(self):
     with open (self,'r',encoding='utf-8') as f:
         for data in f:
@@ -18,7 +18,8 @@ def kingstone_stored(self):
                 print("----------")
 
 
-for filename in os.listdir(os.getcwd()+'\\kingstone_datas\\json'):
-    kingstone_stored(os.getcwd()+'\\kingstone_datas\\json\\'+filename)
-    # print(filename)
+# for filename in os.listdir(os.getcwd()+'\\kingstone_datas\\json'):
+#     kingstone_stored(os.getcwd()+'\\kingstone_datas\\json\\'+filename)
+#     # print(filename)
+kingstone_stored(r"C:\Users\Tibame\Desktop\TFB103-3project\allbooks_update.json")
 print('Completed')
